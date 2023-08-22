f7902f5bc523d7ea5034e56af050541a7b1865f7
Mahdi Khodabandeh <42507906+khoda81@users.noreply.github.com>
2023-08-22 19:01:42 +0330

    Refactored `Fitter` to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,68 @@
+use std::collections;
 
+pub struct Fitter<T> {
+    weights: Vec<T>,
+    bin_capacities: Vec<T>,
+    bins: Vec<Vec<T>>,
+}
+
+impl<T> Fitter<T>
+where
+    T: Ord + Copy + std::hash::Hash + std::ops::SubAssign + std::iter::Sum,
+{
+    pub fn new(mut weights: Vec<T>, bin_capacities: Vec<T>) -> Self {
+        weights.sort();
+
+        Self {
+            bins: vec![Vec::new(); bin_capacities.len()],
+            bin_capacities,
+            weights,
+        }
+    }
+
+    pub fn fit_helper(&mut self) -> bool {
+        let current_item = match self.weights.pop() {
+            Some(x) => x,
+            None => return true,
+        };
+
+        let mut searched_sizes = collections::HashSet::new();
+        for bag_idx in 0..self.bin_capacities.len() {
+            if self.bin_capacities[bag_idx] < current_item {
+                continue;
+            }
+
+            if !searched_sizes.insert(self.bin_capacities[bag_idx]) {
+                continue;
+            }
+
+            // try
+            let old_size = self.bin_capacities[bag_idx];
+            self.bin_capacities[bag_idx] -= current_item;
+            self.bins[bag_idx].push(current_item);
+
+            // recurse
+            if self.fit_helper() {
+                return true;
+            }
+
+            // backtrack
+            self.bins[bag_idx].pop();
+            self.bin_capacities[bag_idx] = old_size;
+        }
+
+        self.weights.push(current_item);
+        false
+    }
+
+    pub fn fit(mut self) -> Option<Vec<Vec<T>>> {
+        let total_weight: T = self.weights.iter().copied().sum();
+        let total_size: T = self.bin_capacities.iter().copied().sum();
+
+        if total_weight <= total_size && self.fit_helper() {
+            Some(self.bins)
+        } else {
+            None
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,6 @@
 use clap::Parser;
-use std::{
-    collections::HashSet,
-    fs,
-    io::{self, Read},
-    path::PathBuf,
-    process::exit,
-    sync::{mpsc, Arc, Mutex},
-    thread,
-    time::Duration,
-};
-
-pub struct Fitter<T> {
-    weights: Vec<T>,
-    bin_capacities: Vec<T>,
-    bins: Vec<Vec<T>>,
-}
-
-impl<T> Fitter<T>
-where
-    T: Ord + Copy + std::hash::Hash + std::ops::SubAssign + std::iter::Sum,
-{
-    pub fn new(mut weights: Vec<T>, bin_capacities: Vec<T>) -> Self {
-        weights.sort();
-
-        Self {
-            bins: vec![Vec::new(); bin_capacities.len()],
-            bin_capacities,
-            weights,
-        }
-    }
-
-    pub fn fit_helper(&mut self) -> bool {
-        let current_item = match self.weights.pop() {
-            Some(x) => x,
-            None => return true,
-        };
-
-        let mut searched_sizes = HashSet::new();
-        for bag_idx in 0..self.bin_capacities.len() {
-            if self.bin_capacities[bag_idx] < current_item {
-                continue;
-            }
-
-            if !searched_sizes.insert(self.bin_capacities[bag_idx]) {
-                continue;
-            }
-
-            // try
-            let old_size = self.bin_capacities[bag_idx];
-            self.bin_capacities[bag_idx] -= current_item;
-            self.bins[bag_idx].push(current_item);
-
-            // recurse
-            if self.fit_helper() {
-                return true;
-            }
-
-            // backtrack
-            self.bins[bag_idx].pop();
-            self.bin_capacities[bag_idx] = old_size;
-        }
-
-        self.weights.push(current_item);
-        false
-    }
-
-    pub fn fit(mut self) -> Option<Vec<Vec<T>>> {
-        let total_weight: T = self.weights.iter().copied().sum();
-        let total_size: T = self.bin_capacities.iter().copied().sum();
-
-        if total_weight <= total_size && self.fit_helper() {
-            Some(self.bins)
-        } else {
-            None
-        }
-    }
-}
+use std::io::Read;
+use std::{fs, io, path, process, sync, thread};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -83,7 +8,7 @@ where
 struct Args {
     /// Input file to parse (uses stdin by default)
     #[arg(short, long)]
-    input_file: Option<PathBuf>,
+    input_file: Option<path::PathBuf>,
 
     /// Timeout for the computation
     #[arg(short, long)]
@@ -115,9 +40,9 @@ fn main() -> anyhow::Result<()> {
         weights.push(nonzero)
     }
 
-    let mutex = Arc::new(Mutex::new(None));
-    let other_fit = mutex.clone();
-    let (tx, rx) = mpsc::channel();
+    let mutex = sync::Arc::new(sync::Mutex::new(None));
+    let other_mutex = mutex.clone();
+    let (tx, rx) = sync::mpsc::channel();
 
     let timeout_thread = thread::spawn(move || {
         if let Some(timeout) = args.timeout {
@@ -129,7 +54,7 @@ fn main() -> anyhow::Result<()> {
             let _ = rx.recv();
         }
 
-        let best_fit: Option<Vec<Vec<_>>> = other_fit.lock().unwrap().take();
+        let best_fit: Option<Vec<Vec<_>>> = other_mutex.lock().unwrap().take();
         match best_fit {
             Some(best_fit) => {
                 println!("s SAT");
@@ -151,7 +76,7 @@ fn main() -> anyhow::Result<()> {
             }
         }
 
-        exit(0);
+        process::exit(0);
     });
 
     let mut max_bins = weights.len();
@@ -159,7 +84,7 @@ fn main() -> anyhow::Result<()> {
     loop {
         println!("c Fitting to {} bins", max_bins);
         let current_fit: Option<Vec<Vec<_>>> =
-            Fitter::new(weights.clone(), vec![bin_capacity; max_bins])
+            fitter::Fitter::new(weights.clone(), vec![bin_capacity; max_bins])
                 .fit()
                 .map(|fit| fit.into_iter().filter(|row| !row.is_empty()).collect());
 
